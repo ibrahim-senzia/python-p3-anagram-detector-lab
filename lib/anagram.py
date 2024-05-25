b3d07300d2176fb91264b676326c36b2b5e79c7d
Ibrahim Senzia <ibrahimsenzia@gmail.com>
2024-05-25 22:09:12 +0300

    anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, possible_anagrams):
+        sorted_word = sorted(self.word)
+        return [anagram for anagram in possible_anagrams if sorted(anagram) == sorted_word]
